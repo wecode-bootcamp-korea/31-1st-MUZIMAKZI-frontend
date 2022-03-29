6b35d812bc10af8ed00cbb6ea7f3ec780c6cf6f5
seo0yoon <genious921225@gmail.com>
2022-03-29 16:45:46 +0900

    Modify: Labo메인페이지 UI완료

--- src/pages/Labo/Labo.js
+++ src/pages/Labo/Labo.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Labo.scss';
 
 const Labo = () => {
-  return <div>dsdasdas</div>;
+  return (
+    <div className="Labo">
+      <div className="container">
+        <div className="laboImg">
+          <img
+            src="http://image.mujikorea.net/display/corner/454/2021-02-15_labo_image_title.jpg"
+            alt="LABO"
+          />
+        </div>
+        <div className="labo">
+          <img
+            className="laboImg_2"
+            src="http://image.mujikorea.net/display/corner/454/220204labo.jpg"
+            alt="LABO"
+          />
+          <span className="laboTitle">무지막지</span>
+          <div className="laboFlex">
+            <p className="laboText">
+              무지막지한 사람들의 사이트 입니다. <br />
+              무지막지에서는 적당한 두께로 <br />
+              계절에 상관없는 의류를 판매합니다. <br />
+            </p>
+            <p className="laboSize">
+              <b>
+                무지막지한 저지 · 풀오버 파카 <br />
+              </b>
+              여성：171cm / 사이즈 XXS～XS / 그레이 <br />
+              남성：184cm / 사이즈 L～XL / 그레이 <br />
+            </p>
+          </div>
+        </div>
+        <div className="viewProductBox">
+          <div className="viewProductBtn">
+            <Link to="/List">MUJI MAKJI 상품 보러가기</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default Labo;
