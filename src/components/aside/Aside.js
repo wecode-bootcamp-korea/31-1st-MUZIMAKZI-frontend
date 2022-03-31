95956226a76697b2791ccc457c992a2609147362
Moonhyeseong <moonhs97@gmail.com>
2022-03-31 13:20:44 +0900

    Modify: 리뷰 수정사항 반영

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -1,38 +1,11 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
 import './Aside.scss';
+
 const Aside = () => {
-  const CATEGOTY_LIST = [
-    {
-      id: 0,
-      name: '남성복',
-      types: [
-        { id: 0, name: '남성 니트' },
-        { id: 1, name: '남성컷앤소' },
-        { id: 2, name: '남성 셔츠' },
-        { id: 3, name: '남성 아웃트탑' },
-        { id: 4, name: '남성 하의' },
-      ],
-    },
-    {
-      id: 1,
-      name: '여성복',
-      types: [
-        { id: 0, name: '여성 니트' },
-        { id: 1, name: '여성컷앤소' },
-        { id: 2, name: '여성 셔츠 • 원피스' },
-        { id: 3, name: '여성 아웃트탑' },
-        { id: 4, name: '여성 보텀' },
-      ],
-    },
-    {
-      id: 2,
-      name: 'MUZI MAKZI',
-      types: [{ id: 0, name: '유니섹스 MAKZI' }],
-    },
-  ];
   return (
-    <div className="Aside">
+    <div className="aside">
       {CATEGOTY_LIST.map(category => {
         return (
           <div className="asideCategory" key={category.id}>
@@ -57,4 +30,33 @@ const Aside = () => {
   );
 };
 
+const CATEGOTY_LIST = [
+  {
+    id: 0,
+    name: '남성복',
+    types: [
+      { id: 0, name: '남성 니트' },
+      { id: 1, name: '남성컷앤소' },
+      { id: 2, name: '남성 셔츠' },
+      { id: 3, name: '남성 아웃트탑' },
+      { id: 4, name: '남성 하의' },
+    ],
+  },
+  {
+    id: 1,
+    name: '여성복',
+    types: [
+      { id: 0, name: '여성 니트' },
+      { id: 1, name: '여성컷앤소' },
+      { id: 2, name: '여성 셔츠 • 원피스' },
+      { id: 3, name: '여성 아웃트탑' },
+      { id: 4, name: '여성 보텀' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'MUZI MAKZI',
+    types: [{ id: 0, name: '유니섹스 MAKZI' }],
+  },
+];
 export default Aside;
