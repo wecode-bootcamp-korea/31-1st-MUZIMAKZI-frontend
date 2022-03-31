68320008cc642fdcfb0d59e58c82da20b61e3953
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-03-31 19:04:54 +0900

    FIX: 공용 nav 구현 완료

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,57 +3,9 @@ import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 const Nav = () => {
-  const inners = [
-    {
-      id: '1',
-      url: '/',
-      value: '인기상품',
-    },
-    {
-      id: '2',
-      url: '/',
-      value: '신상품',
-    },
-    {
-      id: '3',
-      url: '/',
-      value: '온라인 한정사이즈',
-    },
-    {
-      id: '4',
-      url: '/',
-      value: '가격 인하',
-    },
-    {
-      id: '5',
-      url: '/',
-      value: '기간 한정',
-    },
-    {
-      id: '6',
-      url: '/',
-      value: '의복잡화 아울렛',
-    },
-    {
-      id: '7',
-      url: '/',
-      value: '생활잡화 아울렛',
-    },
-  ];
-
-  const innerList = inners.map(inner => {
-    return (
-      <li key={inner.id}>
-        <Link to={inner.url} title={inner.value}>
-          {inner.value}
-        </Link>
-      </li>
-    );
-  });
-
   return (
-    <nav className="Nav">
-      <div className="NavBox">
+    <nav className="nav">
+      <div className="navBox">
         <div className="navTop">
           <a href="/" className="Logo">
             <span className="muziLogo">MUZI</span>
@@ -65,16 +17,30 @@ const Nav = () => {
           </a>
           <div className="navSearch">
             <input className="searchInput" type="text" />
-            <img src="/images/nav/searchIcon.png" alt="" />
+            <img src="/images/nav/searchIcon.png" alt="searchIconImg" />
           </div>
           <div className="linkItem">
-            <button type="button" className="btnLogin" src="/" />
-            <button type="button" className="btnMypage" src="/" />
-            <button type="button" className="btnCart" src="/" />
+            <a href="/sign">
+              <button type="button" className="btnLogin" />
+            </a>
+            <Link to="/">
+              <button type="button" className="btnMypage" />
+            </Link>
+            <Link to="/">
+              <button type="button" className="btnCart" src="/" />
+            </Link>
           </div>
         </div>
         <div className="navBottom">
-          <ul className="inner">{innerList}</ul>
+          <ul className="inner">
+            {INNERS.map(inner => (
+              <li key={inner.id}>
+                <Link to={inner.url} title={inner.value}>
+                  {inner.value}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
@@ -82,3 +48,41 @@ const Nav = () => {
 };
 
 export default Nav;
+
+const INNERS = [
+  {
+    id: '1',
+    url: '/',
+    value: '인기상품',
+  },
+  {
+    id: '2',
+    url: '/',
+    value: '신상품',
+  },
+  {
+    id: '3',
+    url: '/',
+    value: '온라인 한정사이즈',
+  },
+  {
+    id: '4',
+    url: '/',
+    value: '가격 인하',
+  },
+  {
+    id: '5',
+    url: '/',
+    value: '기간 한정',
+  },
+  {
+    id: '6',
+    url: '/',
+    value: '의복잡화 아울렛',
+  },
+  {
+    id: '7',
+    url: '/',
+    value: '생활잡화 아울렛',
+  },
+];
