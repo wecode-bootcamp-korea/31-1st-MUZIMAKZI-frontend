6a1afe917263b2cd635f1688ef2f359de9245053
seo0yoon <genious921225@gmail.com>
2022-03-31 18:34:00 +0900

    Modify: list page수정

---
+++ src/pages/List/ItemList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './List.scss';
+
+function ItemList({ name, price, thumbnail }) {
+  return (
+    <div className="itemContainer">
+      <div className="containerWrap">
+        <div className="listImg">
+          <img src={thumbnail} alt="남자니트" />
+          <div className="listIcons">
+            <img src="/images/listpage/icon_zoom.gif" alt="iconZoom" />
+            <img src="/images/listpage/icon_win.gif" alt="iconWin" />
+          </div>
+        </div>
+        <div className="listTextBox">
+          <div className="listText">{name}</div>
+          <div className="listPrice">
+            <b>{price}</b>원
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemList;
