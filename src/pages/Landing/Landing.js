835b6f94eb946fc235920431bd31b1acb4fce399
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-04-02 18:18:39 +0900

    ADD:first contents style 구현

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { useState, useEffect, useLayoutEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import SwiperData from './SwiperData';
 import './Landing.scss';
 import SliderButton from './SliderButton';
 import Aside from '../../components/aside/Aside';
 import Nav from '../../components/nav/Nav';
+import LandingContents from './LandingContents';
 
 const Landing = () => {
   const [swiperList, setSwiperList] = useState([]);
   // 이미지 리스트
-
   const [currentIndex, setCurrentIndex] = useState(0);
   // 현재 이미지
-
-  // const [count, setCount] = useState(0);
+  const [firstContentsList, setFirstContentsList] = useState([]);
 
   useEffect(() => {
     fetch('data/swiperItems.json')
@@ -21,6 +21,11 @@ const Landing = () => {
       .then(data => setSwiperList(data));
   }, [currentIndex]);
   // mock data 불러옴
+  useEffect(() => {
+    fetch('data/contentslist.json')
+      .then(res => res.json())
+      .then(data => setFirstContentsList(data));
+  }, []);
 
   function handleSlide(currentIndex) {
     if (currentIndex === swiperList.length) {
@@ -36,15 +41,6 @@ const Landing = () => {
   }
   // 상단은 버튼 코드 !
 
-  // const AutoCounter = () => {
-  //   if (currentIndex === swiperList.length) {
-  //     currentIndex = 0;
-  //   } else if (currentIndex < 0) {
-  //     currentIndex = 4;
-  //   }
-  //   setCurrentIndex(currentIndex);
-  // };
-
   useEffect(() => {
     const timeoutId = setTimeout(
       () =>
@@ -59,36 +55,47 @@ const Landing = () => {
     );
     return () => clearTimeout(timeoutId);
   });
-
-  // useEffect(() => {
-  //   const timeoutId = setTimeout(
-  //     () => setCurrentIndex(currentIndex => currentIndex + 1),
-  //     2500
-  //   );
-  //   return () => clearTimeout(timeoutId);
-  // }, [currentIndex]);
+  // 마지막에 배열을 넣지 않은 이유는 clearup을 위한 것
 
   return (
     <div className="landing">
       <Nav />
       <div className="main">
         <Aside />
-        <div className="sliderArea">
-          <div className="slider">
-            <SliderButton direction="prev" onClick={() => handleSwipe(-1)} />
-            <SliderButton direction="next" onClick={() => handleSwipe(1)} />
-            <div className="sliderList">
-              <div
-                className="sliderTrack"
-                style={{
-                  transform: `translateX(${-870 * (0.5 + currentIndex)}px)`,
-                }}
-              >
-                {swiperList.map(imgList => {
-                  return <SwiperData key={imgList.id} {...imgList} />;
-                })}
+        <div className="contents">
+          <div className="sliderArea">
+            <div className="slider">
+              <SliderButton direction="prev" onClick={() => handleSwipe(-1)} />
+              <SliderButton direction="next" onClick={() => handleSwipe(1)} />
+              <div className="sliderList">
+                <div
+                  className="sliderTrack"
+                  style={{
+                    transform: `translateX(${-870 * (0.5 + currentIndex)}px)`,
+                  }}
+                >
+                  {swiperList.map(imgList => {
+                    return <SwiperData key={imgList.id} {...imgList} />;
+                  })}
+                </div>
               </div>
             </div>
+          </div>
+          <div className="firstContents">
+            <div className="firstTop">
+              <span>기획전</span>
+              <Link to="/">
+                <button className="firstTopBtn" />
+              </Link>
+            </div>
+            <ul className="firstBottom">
+              {firstContentsList.map(contentList => {
+                // console.log(contentList);
+                return (
+                  <LandingContents key={contentList.id} {...contentList} />
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
@@ -119,4 +126,13 @@ export default Landing;
 //     </div>
 //   </div>
 // );
+// };
+
+// const AutoCounter = () => {
+//   if (currentIndex === swiperList.length) {
+//     currentIndex = 0;
+//   } else if (currentIndex < 0) {
+//     currentIndex = 4;
+//   }
+//   setCurrentIndex(currentIndex);
 // };
---
+++ src/pages/Landing/LandingContents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingContents.scss';
+
+const LandingContents = ({ img_url, title, content_value }) => {
+  return (
+    <li className="firstContent">
+      <img src={img_url} alt={title} />
+      <div>
+        <span>{content_value}</span>
+      </div>
+    </li>
+  );
+};
+
+export default LandingContents;
