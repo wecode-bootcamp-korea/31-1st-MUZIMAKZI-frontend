b67c88e44b71d76a70777f306e559bf92b31eb2d
Jaehyuksssss <ljh9510@naver.com>
2022-04-04 18:12:40 +0900

    sider 오류 반영

--- src/pages/Mens/MensComponent/MensList.js
+++ src/pages/Mens/MensComponent/MensList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MensList = ({ id, name, thumbnail }) => {
   return (
     <div className="smallList">
-      <p>{name}</p> <img className="tShirt" src={thumbnail} alt=""></img>
+      <p>{name}</p> <img className="tShirt" src={thumbnail} alt="" />
     </div>
   );
 };
