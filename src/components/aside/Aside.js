c279248c4c83ece9fe003a6bbbc5a51cf5924697
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-04-05 13:42:34 +0900

    ADD:패스파라미터 적용

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -1,22 +1,32 @@
 import React from 'react';
-
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Aside.scss';
 
 const Aside = () => {
+  const [categoryList, setCategoryList] = useState(0);
+
+  useEffect(() => {
+    fetch('data/categorylist.json')
+      .then(res => res.json())
+      .then(data => {
+        setCategoryList(data);
+      });
+  }, []);
+
   return (
     <div className="aside">
-      {CATEGOTY_LIST.map(category => {
+      {categoryList.map(category => {
         return (
           <div className="asideCategory" key={category.id}>
             <h2 className="categoryName">
-              <Link to="/mens">{category.name}</Link>
+              <Link to={`${category.linkurl}`}>{category.name}</Link>
             </h2>
             <ul className="categoryTypes">
               {category.types.map(types => {
                 return (
                   <li className="categoryType" key={types.id}>
-                    <Link to="/mens" className="typeLink">
+                    <Link to={`category=${types.id}`} className="typeLink">
                       {types.name}
                     </Link>
                   </li>
@@ -31,33 +41,3 @@ const Aside = () => {
 };
 
 export default Aside;
-
-const CATEGOTY_LIST = [
-  {
-    id: 0,
-    name: '남성복',
-    types: [
-      { id: 0, name: '남성 니트' },
-      { id: 1, name: '남성컷앤소' },
-      { id: 2, name: '남성 셔츠' },
-      { id: 3, name: '남성 아웃트탑' },
-      { id: 4, name: '남성 하의' },
-    ],
-  },
-  {
-    id: 1,
-    name: '여성복',
-    types: [
-      { id: 0, name: '여성 니트' },
-      { id: 1, name: '여성컷앤소' },
-      { id: 2, name: '여성 셔츠 • 원피스' },
-      { id: 3, name: '여성 아웃트탑' },
-      { id: 4, name: '여성 보텀' },
-    ],
-  },
-  {
-    id: 2,
-    name: 'MUZI MAKZI',
-    types: [{ id: 0, name: '유니섹스 MAKZI' }],
-  },
-];
