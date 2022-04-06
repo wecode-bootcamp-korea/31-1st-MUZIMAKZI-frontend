c17f4cfa1c1d0fc261ce1cfa5ccde3ad6711d418
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-04-06 17:47:36 +0900

    FIX: 사이더 수정 완료

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 // import { type } from 'sass';
-import { Link, useParams, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Aside.scss';
 
 const Aside = () => {
   const [categoryList, setCategoryList] = useState([]);
   const navigate = useNavigate();
-  const location = useLocation();
 
   useEffect(() => {
-    fetch('http://10.58.1.246:8000/main')
+    fetch('/data/categorylist.json')
       .then(res => res.json())
       .then(data => {
-        setCategoryList(data.side_info);
+        setCategoryList(data.side);
       });
   }, []);
 
