9c44d937f900abf3cc62fac959a391727f314da4
seo0yoon <genious921225@gmail.com>
2022-04-07 17:39:39 +0900

    modify: 폴더명수정

--- src/pages/List/SelectBoxComponents/selectBox.js
+++ src/pages/List/SelectBoxComponents/selectBox.js
@@ -2,7 +2,6 @@ import React from 'react';
 import SelectText from '../SelectTextComponents/SelectText';
 import './SelectBox.scss';
 
-console.log('dd');
 const SelectBox = () => {
   return (
     <div className="selectBox">
