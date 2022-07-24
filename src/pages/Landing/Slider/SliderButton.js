bbb9d8816ac99c502abc9616628d13c792985207
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-07-24 21:16:25 +0900

    Modify: 랜딩 슬라이드 스타일 수정

--- src/pages/Landing/Slider/SliderButton.js
+++ src/pages/Landing/Slider/SliderButton.js
@@ -3,3 +3,5 @@ import './SliderButton.scss';
 export default function SlideButton({ direction, onClick }) {
   return <button onClick={onClick} className={`btn${direction}`} />;
 }
+FIX ME: 슬라이드 수정 dsadsadsa
+sadsadsadsadsadsadsa
